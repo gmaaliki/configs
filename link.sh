ed775db2e192dcf2c611ead4500d8b1846ed7aef
gmaaliki <maalikighifari@gmail.com>
2023-05-30 19:23:46 +0700

    feat: adding picom symlink

--- link.sh
+++ link.sh
@@ -1,9 +1,10 @@
 ## Create symlinks for the configs
-ln -s ./i3 ~/.config/i3;
-ln -s ./nvim ~/.config/nvim;
-ln -s ./.tmux.conf ~/.tmux.conf;
-ln -s ./.Xresources ~/.Xresources;
-ln -s ./.zshrc ~/.zshrc;
+ln -s ./i3 ~/.config/i3
+ln -s ./nvim ~/.config/nvim
+ln -s ./picom ~/.config/picom
+ln -s ./.tmux.conf ~/.tmux.conf
+ln -s ./.Xresources ~/.Xresources
+ln -s ./.zshrc ~/.zshrc
 
 ## Install the necessary programs
 yes J | sudo pacman -S i3 i3status neovim rxvt-unicode zsh picom feh xsel xorg-xrdb curl
